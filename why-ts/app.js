4e859e237ab1e3afad761859c8e80260e59363a7
osunguk(mac) <osunguk@gmail.com>
2021-04-02 00:47:57 +0900

    🔨타입 스크립트가 없을 때, 파라미터로 오는 타입 체크하는 방법

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,13 +1,38 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
+
+/**
+ * @typedef {object} address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {object} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then((res) => {
+  console.log(res);
+  console.log(res.name);
+});
 
 function startApp() {
   axios
@@ -16,6 +41,9 @@ function startApp() {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      username.textContent = user.username;
+      email.textContent = user.email;
+      address.textContent = user.address.street;
     })
     .catch(function (error) {
       console.log(error);
